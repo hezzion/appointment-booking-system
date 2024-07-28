78d658cd7823db21b71ea5b0e19e2fdccd7abc92
unknown <zachijonathan3@gmail.com>
2024-07-29 00:07:24 +0100

    user profile update

--- Admin/index.php
+++ Admin/index.php
@@ -238,12 +238,7 @@ aria-hidden="true">
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
                                
-                                <div class="dropdown-divider"></div>
                                 <button class="dropdown-item" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
